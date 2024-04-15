bb730a87da0c4a5958936ea7ce47d05c4cae4b2b
pbasimfa <pbasimfa@volvocars.com>
2024-04-15 14:53:12 +0200

    Add module calc and module structure to project

---
+++ calc/calculator.go
@@ -0,0 +1,17 @@
+package calc
+
+func Add(x int, y int) int {
+	return x + y
+}
+
+func Subtract(x int, y int) int {
+	return x - y
+}
+
+func Multiply(x int, y int) int {
+	return x * y
+}
+
+func Divide(x float64, y float64) float64 {
+	return x / y
+}
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"learn-go/calc"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println(calc.Add(1, 2), "Addition")
+	fmt.Println(calc.Subtract(1, 2), "Subtraction")
+	fmt.Println(calc.Multiply(1, 2), "Multiplication")
+	fmt.Println(calc.Divide(1, 2), "Division")
+}
