3e3cef780e5700d67c1bff3f62f8c2e5250dd06a
pbasimfa <pbasimfa@volvocars.com>
2024-05-02 12:41:56 +0200

    Add the factorial func to calc

--- calc/calculator.go
+++ calc/calculator.go
@@ -15,3 +15,17 @@ func Multiply(x int, y int) int {
 func Divide(x float64, y float64) float64 {
 	return x / y
 }
+
+func Factorial(x int) int {
+	// This calculates the factorial of an integer and also
+	// handles negative values by returning an error type.
+
+	if x < 0 {
+		return -1
+	}
+	product := 1
+	for i := 1; i <= x; i++ {
+		product *= i
+	}
+	return product
+}
